581467bc43af9bae8e2a8be4c06fb108845514c8
syntroy <syntroi@outlook.com>
2025-07-21 03:38:12 -0700

    refactor(shared-admin-db): Use DotEnv variable expansion to setup database url variable

--- shared/api/db/admin-db/prisma/seeds/initial-data.ts
+++ shared/api/db/admin-db/prisma/seeds/initial-data.ts
@@ -1,9 +1,7 @@
 import postgres from 'postgres'
 
 export const createInitialData = async () => {
-  const prismaURL = process.env.DATABASE_URL
-  const url = prismaURL.substring(0, prismaURL.indexOf('?')) // remove the 'schema' parameter of the database URL
-  const sql = postgres(url)
+  const sql = postgres(process.env.DATABASE_URL)
   const sqlFilePath = new URL('./data.sql', import.meta.url).pathname
 
   await sql.file(sqlFilePath)

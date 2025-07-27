3400cdfc6b16a064fa0a420363d03ddac899b896
syntroy <syntroi@outlook.com>
2025-07-27 00:46:14 -0700

    feat: Add new ui type utility functions to easily obtain types from the generated open-api types

--- shared/ui/types/utility.d.ts
+++ shared/ui/types/utility.d.ts
@@ -17,3 +17,55 @@ export type UnwrapMaybeRefOrGetter<T> = T extends () => infer R
 export type TreeList<T, D = T & { disabled?: boolean }> = (D & {
   children: D[]
 })[]
+
+export type GetData<Paths, P extends keyof Paths> = Paths extends {
+  [k: string]: any
+}
+  ? Paths[P] extends {
+      get: {
+        responses: {
+          200: {
+            content: {
+              'application/json': {
+                data: infer D
+              }
+            }
+          }
+        }
+      }
+    }
+    ? D
+    : never
+  : never
+
+export type PostReqBody<Paths, P extends keyof Paths> = Paths extends {
+  [k: string]: any
+}
+  ? Paths[P] extends {
+      post: {
+        requestBody: {
+          content: {
+            'application/json': infer B
+          }
+        }
+      }
+    }
+    ? B
+    : never
+  : never
+
+export type PatchReqBody<Paths, P extends keyof Paths> = Paths extends {
+  [k: string]: any
+}
+  ? Paths[P] extends {
+      patch: {
+        requestBody: {
+          content: {
+            'application/json': infer B
+          }
+        }
+      }
+    }
+    ? B
+    : never
+  : never

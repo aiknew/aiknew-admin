16eeb0f774442348f6d6a7a0b21a0be0bd4a18d5
syntroy <syntroi@outlook.com>
2025-08-18 06:16:50 -0700

    chore(admin-ui): Remove unused code

--- apps/admin-ui/src/stores/lang.ts
+++ apps/admin-ui/src/stores/lang.ts
@@ -1,7 +1,6 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { LanguageData } from '@/api/language'
-import { z } from 'zod'
 
 export const useLangStore = defineStore('lang', () => {
   const enabledLangs = ref<LanguageData[]>([])
@@ -12,29 +11,8 @@ export const useLangStore = defineStore('lang', () => {
     return enabledLangs.value.find((lang) => lang.key === key)?.name
   }
 
-  const buildTranslationSchema = <T extends z.ZodTypeAny>(
-    defaultSchema: T,
-    defaultVal?: z.infer<T>
-  ) => {
-    const defaultVals: Record<string, z.infer<T>> = {}
-    const o: Record<string, z.ZodTypeAny> = {}
-    for (const lang of enabledLangs.value) {
-      defaultVals[lang.key] = defaultVal
-      o[lang.key] = defaultSchema
-    }
-    // const baseSchema = z.record(z.string(), defaultSchema)
-    const baseSchema = z.object(o)
-
-    if (defaultVal) {
-      return baseSchema.default(defaultVals)
-    }
-
-    return baseSchema
-  }
-
   return {
     enabledLangs,
-    getNameByKey,
-    buildTranslationSchema
+    getNameByKey
   }
 })

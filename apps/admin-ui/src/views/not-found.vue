f0575fac3e765f6685850481410df1c890ecdd58
syntroy <syntroi@outlook.com>
2025-09-12 01:06:42 -0700

    refactor: Update translations for not-found page

--- apps/admin-ui/src/views/not-found.vue
+++ apps/admin-ui/src/views/not-found.vue
@@ -1,37 +1,18 @@
 <script lang="ts" setup>
 import { ElButton, ElResult } from 'element-plus'
-import { useI18n } from 'vue-i18n';
-import { useRouter } from 'vue-router';
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
 
 const { push } = useRouter()
 const { t } = useI18n()
 </script>
 
 <template>
-  <el-result title="404" :sub-title="t('tips')">
+  <el-result title="404" :sub-title="t('notFound.tips')">
     <template #extra>
       <el-button type="primary" @click="push('/')">
-        {{ t('backToHome') }}
+        {{ t('notFound.backToHome') }}
       </el-button>
     </template>
   </el-result>
 </template>
-
-<i18n>
-  {
-    "en": {
-      "tips": "The page was not found",
-      backToHome: 'Back to homepage'
-    },
-
-    "zh-CN": {
-      "tips": "没有找到该页面",
-      backToHome: '回到首页'
-    },
-
-    "zh-TW": {
-      "tips": "沒有找到該頁面",
-      backToHome: '回到首頁'
-    }
-  }
-</i18n>
